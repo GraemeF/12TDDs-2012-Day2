f54aa8c0eada76a3519ba32ce03bc85cd43315b5
Graeme Foster <graemef@gmail.com>
2012-12-29 22:05:49 +0000

    Removed duplication.

--- test.js
+++ test.js
@@ -2,10 +2,12 @@ var convert = require('./index');
 require('chai').should();
 
 describe('Number names', function() {
-  it('should convert 0 to zero', function() {
-    convert(0).should.equal('zero');
-  });
-  it('should convert 1 to one', function() {
-    convert(1).should.equal('one');
-  });
+  var describeSingleDigit = function(n, expected) {
+      it('should convert ' + n + ' to ' + expected, function() {
+        convert(n).should.equal(expected);
+      });
+    };
+
+  describeSingleDigit(0, 'zero');
+  describeSingleDigit(1, 'one');
 });
